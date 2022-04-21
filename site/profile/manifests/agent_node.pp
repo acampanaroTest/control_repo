1b35fac92b807c1b41294c8e8c72b9ff47b632bd
acampanaroTest <104109393+acampanaroTest@users.noreply.github.com>
2022-04-21 18:50:24 +1000

    Update agent_node.pp

--- site/profile/manifests/agent_node.pp
+++ site/profile/manifests/agent_node.pp
@@ -2,10 +2,4 @@ class profile::agent_nodes {
   include dockeragent
   dockeragent::node { 'web.puppet.vm': }
   dockeragent::node { 'db.puppet.vm': }
-  host {‘web.puppet.vm’:
-    ip => ‘172.18.0.2’,
-  }
-  host {‘db.puppet.vm’:
-    ip => ‘172.18.0.3’,
-  }
 }
