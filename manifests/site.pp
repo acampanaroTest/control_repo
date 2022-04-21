7a28015d19ec937dcdb0f18547740ad28c8ea382
acampanaroTest <104109393+acampanaroTest@users.noreply.github.com>
2022-04-21 18:26:32 +1000

    Update site.pp

--- manifests/site.pp
+++ manifests/site.pp
@@ -1,14 +1,19 @@
 node default {
-  
+  file { '/root/README':
+    ensure => file,
+    content => 'This is a readme',
+    owner   => 'root',
+  }
 }
 node 'master.puppet.vm' {
   include role::master_server
   file {'/root/README':
     ensure => file,
-    content => "Welcome to ${fqdn}\n",
+    content => “Welcome to ${fqdn}”,
+    owner => 'root',
   }
 }
-node /^web/ {
+node /^web/ { 
   include role::app_server
 }
 node /^db/ {
