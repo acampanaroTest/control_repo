d0e7eabe38415da93afe4db9fc284c75ba939e83
acampanaroTest <104109393+acampanaroTest@users.noreply.github.com>
2022-04-22 09:35:52 +1000

    Update ssh_server.pp

--- site/profile/manifests/ssh_server.pp
+++ site/profile/manifests/ssh_server.pp
@@ -1,13 +1,12 @@
 class profile::ssh_server {
    package {'openssh-server':
-    ensure => present,
+      
   }
     service {'sshd':
       ensure => 'running',
       ensure => 'true',      
     }
     ssh_authorized_key {'root@master.puppet.vm':
-      ensure => present,
       user => 'root',
       type => 'ssh-rsa',
       key => 'AAAAB3NzaC1yc2EAAAADAQABAAABAQDPh/UmM9+9z6K76I/2Yx2cFQw/XJLZr392zDld0OJPsw7eTV6GNkn2R7DNWQh3Uglka5TxTI0VdeEsfiPRgapx4T1CO60RO43AQ2ei9IUq1UvxIIEd2Bn0roXKFqK/8CcUW8E38u9nis0kkmdeoyBOVAxEa28471+lLdnV1K+SE3DsSMcMWO6X3L4AGq05eiIuy3YOd47IhFCFchU5xqm9jPA2yfZZ0qvwV+X63ahdsjmrD4HF+q7kLTgCXfpU2dBpbQK4X4rH3NtIdUlq5i39rYopzgB/3IDLf6CyDO8VY4El6TceupU1Vln6uiRwVr4YePr1tjKM97XFE1vvdQun',
